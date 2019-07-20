ad172200859e78fa5cb6e598498ea56ff03c3062
Joseph Post <joe@jcpst.com>
2019-07-20 11:47:50 -0500

    use fractions in interval! macro

--- src/intervals.rs
+++ src/intervals.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! interval {
-    ($name:ident, $num:tt, $den:tt) => {
+    ($name:ident, $num:tt / $den:tt) => {
         interval!($name, $num, $den, stringify!($num), stringify!($den));
     };
 
@@ -14,20 +14,20 @@ macro_rules! interval {
     }
 }
 
-interval!(TONIC, 1, 1);
-interval!(SYNTONIC_COMMA, 81, 80);
-interval!(PTOLEMYS_ENHARMONIC, 56, 55);
+interval! { TONIC, 1/1 }
+interval! { SYNTONIC_COMMA, 81/80 }
+interval! { PTOLEMYS_ENHARMONIC, 56/55 }
 
-interval!(SEPTIMAL_COMMA, 36, 35);
-interval!(UNDECIMAL_COMMA, 33, 32);
-interval!(SIXTY_FIFTH_HARMONIC, 65, 64);
+interval! { SEPTIMAL_COMMA, 36/35 }
+interval! { UNDECIMAL_COMMA, 33/32 }
+interval! { SIXTY_FIFTH_HARMONIC, 65/64 }
 
-interval!(OCTAVE, 2, 1);
+interval! { OCTAVE, 2/1 }
 
-interval!(C, 1, 1);
-interval!(D, 9, 8);
-interval!(E, 5, 4);
-interval!(F, 4, 3);
-interval!(G, 3, 2);
-interval!(A, 5, 3);
-interval!(B, 15, 8);
+interval! { C, 1/1 }
+interval! { D, 9/8 }
+interval! { E, 5/4 }
+interval! { F, 4/3 }
+interval! { G, 3/2 }
+interval! { A, 5/3 }
+interval! { B, 15/8 }
